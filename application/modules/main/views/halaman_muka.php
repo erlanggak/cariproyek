8ecc3ba73183c0da7c973424bc67ed878d4d8e9d
Al Aji <alajiseno@gmail.com>
2015-08-19 21:15:39 +0700

    kick off cariproyek

---
+++ application/modules/main/views/halaman_muka.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+	<title>Halaman Utama Kidokado.com</title>
+</head>
+<body>
+	<?php ?>
+		<div>
+			<input>
+			<input>
+			<button>Login</button>
+		</div>
+</body>
+</html>
---
+++ application/modules/products/views/halaman_produk.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+	<title>Halaman Produk</title>
+</head>
+<body>
+	<?php ?>
+		<div>
+			<input>
+			<input>
+			<button>Login</button>
+		</div>
+</body>
+</html>
