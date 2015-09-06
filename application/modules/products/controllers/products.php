649aecd85435c9051b7268634c906f6739f51d28
Dwi Al Aji Suseno <alajiseno@gmail.com>
2015-09-06 16:44:33 +0700

    test new

--- application/modules/products/controllers/products.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Products extends CI_Controller {
-	public function index(){
-		$this->load->view('halaman_produk');
-	}
-
-	
-}
